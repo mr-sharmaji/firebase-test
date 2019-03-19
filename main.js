30aa3b89c9f22258e2534613840e976396f807fa
Subham Sharma <33781108+mr-sharmaji@users.noreply.github.com>
2019-03-20 04:06:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-alert("Connected");
+
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         // User is signed in.
         document.getElementById("user_div").style.display = "block";
         document.getElementById("login_div").style.display = "none";
+        
+        var user = firebase.auth().currentUser;
+        var emailid = document.getElementById("email").value;
+        if(user!=null){
+            var emailId= document.getElementById("email").value;
+            document.getElementById("user-text").innerHTML =emailId;
+        }
+        
     } else {
         // No user is signed in.
         document.getElementById("user_div").style.display = "none";
@@ -23,4 +31,18 @@ function login(message) {
         window.alert("error: " + errorMessage);
     });
 
+}
+
+function logout() {
+    firebase.auth().signOut().then(function() {
+        var userEmail = document.getElementById("email").value;
+        // Sign-out successful.
+        window.alert(userEmail + " Has logged out successfully");
+
+    }).catch(function(error) {
+        // An error happened.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        window.alert("error: " + errorMessage);
+    });
 }
